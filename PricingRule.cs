526e3d5c830269ed769738f6a205e7b4c750db6a
bhupender <bhupyy@users.noreply.github.com>
2025-01-14 23:11:57 +0000

    Class defining PricingRule components is added

---
+++ PricingRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket
+{
+    public class PricingRule
+    {
+
+        public string Sku { get; set; }
+        public int UnitPrice { get; set; }
+        public int SpecialQuantity { get; set; }
+        public int SpecialPrice { get; set; }
+
+    }
+}
